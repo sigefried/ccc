dbe69986979bb64b01d31ab66ad73c285da68aae
sigefried <sigefriedhyy@gmail.com>
2018-10-06 12:39:05 +0900

    update

--- task2/submit_task-yarn.sh
+++ task2/submit_task-yarn.sh
@@ -5,6 +5,9 @@ script=$1
 
 spark-submit \
 --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.2.2 \
---master local[2] \
+--master yarn \
 --deploy-mode client \
+--conf spark.task.cpus=1 \
+--conf spark.yarn.executor.memoryOverhead=2000 \
+--conf spark.default.parallelism=21 \
 $script
