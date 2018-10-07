35608873ac436b6fc5fd9b8ed18c16acd9ee2ce2
sigefriedhyy <sigefriedhyy@gmail.com>
2018-10-07 13:58:50 +0000

    update

--- task2/submit_task-yarn.sh
+++ task2/submit_task-yarn.sh
@@ -7,6 +7,11 @@ spark-submit \
 --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.2.2 \
 --master yarn \
 --deploy-mode client \
+--driver-memory 4g \
+--driver-cores 4 \
+--executor-memory 3g \
+--num-executors 10 \
+--executor-cores 2 \
 --conf spark.task.cpus=1 \
 --conf spark.yarn.executor.memoryOverhead=2000 \
 --conf spark.default.parallelism=21 \
